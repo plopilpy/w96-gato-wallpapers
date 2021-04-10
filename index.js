12a3862c6c0f35a570367aa871d96f5122e8b884
RLX <r@yaboi.wtf>
2021-04-09 23:26:25 -0400

    yes good

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+/*
+Windows 96 PackMan repository server.
+
+Copyright (C) Windows 96 Team (windows96.net) 2021.
+*/
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
+const maincfg = require('./configs/main.json');
 
-app.use(cors());
-app.use('/', express.static("public"));
+if(maincfg.server.corsPolicy == "permissive")
+    app.use(cors());
 
-app.listen(2002, ()=>{
-    console.log("Serving...");
+for(let mp of maincfg.repo.customUrlMappings) {
+    app.use(mp.url, express.static(mp.target));
+}
+
+app.use('/', express.static(maincfg.repo.root_dir));
+
+app.listen(maincfg.server.port, ()=>{
+    console.log(`PackMan Repository Server version 1.0\nCopyright (C) Windows 96 Team 2021.\n`);
+    console.log(`Listening at port [::${maincfg.server.port}]`);
+    console.log(`\n<Server Parameters>`);
+    console.log(` Repository Root: ${path.resolve(maincfg.repo.root_dir)}`);
+    console.log(` CORS policy: ${maincfg.server.corsPolicy}`);
 });
